f38e9c8af968a52be88ba69f7fe124ed8d9dcc53
Jeyther <jeyther13@gmail.com>
2022-04-21 11:20:42 -0500

    seccion nosotros y carrusel

--- js/index.js
+++ js/index.js
@@ -1,10 +1,41 @@
-/* $(() => {
+$(() => {
 
-    $('.seccion-2__namaste__carrusel').slick({
+    $('#carrusel').slick({
+        dots: false,
+        arrows: true,
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 
 })
- */
