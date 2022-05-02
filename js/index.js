0be4caf3619fdd352b26f9de9ee84a8758408736
Jeyther <jeyther13@gmail.com>
2022-05-02 17:57:55 -0500

    carrusel modificado y animaciones elimindas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+$(() => {
+
+
     let productos;
 
     const URLJSON = "productos.json";
@@ -49,6 +52,8 @@
 
         });
 
+        cargarCarrusel(".carrusel--namaste");
+
     }
 
     const cargarProductosVamp = () => {
@@ -81,9 +86,53 @@
 
         });
 
-    }
-    
+        cargarCarrusel(".carrusel--vamp");
 
+    }
+
+    const cargarCarrusel = (tienda) => {
+
+        $(tienda).slick({
+            dots: false,
+            arrows: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 700,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
+    }
+
+});
 /*     $("#submit").click((e) => {
 
         e.preventDefault();
