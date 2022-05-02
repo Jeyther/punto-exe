3d0aef27099d74b11f2cab828b17b386fe654e77
Jeyther <jeyther13@gmail.com>
2022-05-02 00:38:54 -0500

    añadiendo animacion de scroll y separando scripts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-$(() => {
-
-
     let productos;
 
     const URLJSON = "productos.json";
@@ -52,8 +49,6 @@ $(() => {
 
         });
 
-        animarCarrusel(".carrusel--namaste");
-
     }
 
     const cargarProductosVamp = () => {
@@ -86,51 +81,8 @@ $(() => {
 
         });
 
-        animarCarrusel(".carrusel--vamp");
-
     }
-
-    const animarCarrusel = (tienda) => {
-
-        $(tienda).slick({
-            dots: false,
-            arrows: true,
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        infinite: true
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-            ]
-        });
-
-    }
+    
 
 /*     $("#submit").click((e) => {
 
@@ -191,4 +143,3 @@ $(() => {
         }
     });
  */
-})
